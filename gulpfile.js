9534f3922197fa29153fcef86a7d45a1934a9f15
zjy <3152811479@qq.com>
2018-11-05 09:51:50 +0800

    压缩合并完成

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,36 @@
  * @Author: zjy 
  * @Date: 2018-11-05 08:51:52 
  * @Last Modified by: zjy
- * @Last Modified time: 2018-11-05 09:31:45
+ * @Last Modified time: 2018-11-05 09:50:32
  */
-
+// 下载并引入
 var gulp = require("gulp");
 var fs = require("fs");
 var path = require("path");
 var sass = require("gulp-sass");
 var server = require("gulp-webserver");
+var clean = require("gulp-clean-css");
+var uglify = require("gulp-uglify");
+var babel = require("gulp-babel");
+// 编译scss
 gulp.task("source", function() {
     return gulp.src("src/scss/index.scss")
         .pipe(sass())
+        .pipe(clean())
         .pipe(gulp.dest("src/css"))
 })
+
+// 压缩js
+gulp.task("uglify", function() {
+    return gulp.src("src/js/index.js")
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(uglify())
+        .pipe(gulp.dest("build/js"))
+})
+
+// 起服务
 gulp.task("server", function() {
     return gulp.src("src")
         .pipe(server({
@@ -35,6 +52,8 @@ gulp.task("server", function() {
             }
         }))
 })
+
+// 监听
 gulp.task("watch", function() {
     return gulp.watch("src/scss/index.scss", gulp.series("source"))
 })
