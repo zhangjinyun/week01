5c0f575f9e80c72d3790a50fafaa593cb61141c2
zjy <3152811479@qq.com>
2018-11-05 09:16:05 +0800

    起服务

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,12 @@
  * @Author: zjy 
  * @Date: 2018-11-05 08:51:52 
  * @Last Modified by: zjy
- * @Last Modified time: 2018-11-05 09:01:14
+ * @Last Modified time: 2018-11-05 09:15:29
  */
 
 var gulp = require("gulp");
+var fs = require("fs");
+var path = require("path");
 var sass = require("gulp-sass");
 var server = require("gulp-webserver");
 gulp.task("source", function() {
@@ -13,8 +15,27 @@ gulp.task("source", function() {
         .pipe(sass())
         .pipe(gulp.dest("src/css"))
 })
-
+gulp.task("server", function() {
+    return gulp.src("src")
+        .pipe(server({
+            port: 9090,
+            // open: true,
+            middleware: function(req, res, next) {
+                var pathname = require("url").parse(req.url, true).pathname;
+                if (pathname === "/favicon.ico") {
+                    return res.end();
+                }
+                if (pathname === "") {
+                    // 接口
+                } else {
+                    // 静态资源
+                    pathname = pathname === "/" ? "index.html" : pathname;
+                    res.end(fs.readFileSync(path.join(__dirname, "src", pathname)))
+                }
+            }
+        }))
+})
 gulp.task("watch", function() {
     return gulp.watch("src/scss/index.scss", gulp.series("source"))
 })
-gulp.task("dev", gulp.series("watch"))
+gulp.task("dev", gulp.series("server", "watch"))
